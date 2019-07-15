0afe6e75f15f2c5dcfabd7d1462f91cee17e8e54
key0123456789 <k.i-0123456789@live.jp>
2019-07-15 21:46:21 +0900

    Add column user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_141051) do
+ActiveRecord::Schema.define(version: 2019_07_14_142930) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_14_141051) do
     t.text "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
